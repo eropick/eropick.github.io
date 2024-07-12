1b68ecb6f82adb89c935438b4a16350e89489bbe
eropick <kt092805@gmail.com>
2024-07-12 16:08:52 +0900

    add Elevator Layout

--- Elevator(미구현)/elevator_ui.js
+++ Elevator(미구현)/elevator_ui.js
@@ -1,12 +1,19 @@
 /* ui 관련 설정 부분 */
+$(document).ready(()=>{
+    $(".door .door_left").hover(function(e){
+        /* hover 이벤트 발생. */
+        
+    });
+});
 
+/* 엘리베이터 버튼 */
 const ebtns=document.querySelectorAll(".ebtn");
 
-window.addEventListener("load",()=>{
+window.onload = ()=>{
     //버튼 처리
     ebtns.forEach((btn)=>{
         btn.addEventListener("click",()=>{
             btn.classList.toggle("select");
         })
-    })
-})
+    });
+};
