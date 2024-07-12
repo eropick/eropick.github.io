502181ed197409f274a92fa85f785ad5532bfc5a
eropick <kt092805@gmail.com>
2024-07-12 17:22:33 +0900

    add elevator animation

--- Elevator(미구현)/elevator_ui.js
+++ Elevator(미구현)/elevator_ui.js
@@ -1,8 +1,24 @@
 /* ui 관련 설정 부분 */
 $(document).ready(()=>{
-    $(".door .door_left").hover(function(e){
+    let dl_width = $(".door .door_left").css("width");
+    let dr_width = $(".door .door_right").css("width");
+
+    $(".door").hover(function(){    
         /* hover 이벤트 발생. */
-        
+        $(".door .door_left").animate({
+            width : "1px",
+        },3000);
+        $(".door .door_right").animate({
+            width : "1px",
+        },3000);
+    },function(){
+        /* hover 이벤트 발생. */
+        $(".door .door_left").animate({
+            width : dl_width,
+        },3000);
+        $(".door .door_right").animate({
+            width : dr_width,
+        },3000);
     });
 });
 
