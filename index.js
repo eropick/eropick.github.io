18b6c198cfcc68a025cec79801dc3a765cd62c4c
eropick <34877404+eropick@users.noreply.github.com>
2024-07-22 20:04:57 +0900

    페이지 제작중

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-window.addEventListener("load",function(){
+$(document).ready(function(){
     
+    //사이드바 애니메이션 등록
+    $('.sidebar button[type="button"]').click(function(){
+        /* 원위치 기준으로 어디로 이동할지*/
+        $(".sidebar").toggleClass("sidebar_toggle");
+        let pushed=$(".sidebar").hasClass("sidebar_toggle");
+        if(pushed){
+            $(".sidebar").animate({
+                "translate" : "-85%",
+            },1000);
+        }
+        else{
+            $(".sidebar").animate({
+                "translate" : "0%",
+            },1000);
+        }
+    });
 });
